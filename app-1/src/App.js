2c064c98c85646d2d874e71b34dc19dfbb07a07f
tjorge30 <mrs.javalera@gmail.com>
2021-03-30 21:41:04 -0600

    app-1 complete

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import List from './Components/List';
+import AddTodo from './Components/AddTodo';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([])
+
+  function addTodo(item) {
+    const newTodos = [...todos, item]
+    setTodos(newTodos)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='listArea'>
+      <h1>My Todo List</h1>
+      <AddTodo addTodo={addTodo} />
+      <List list={todos} />
+    </div>
     </div>
   );
 }
